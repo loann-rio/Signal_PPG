704106846d358a42d8db048660427cfd69dbb5bf
rio.loann <rio.loann@gmail.com>
2023-02-06 15:10:40 +0100

    added frequency

--- Main.m
+++ Main.m
@@ -5,11 +5,11 @@
 
 clc #Clear terminal
 
-data = dlmread('save_list_colors.txt',''); #-----------------------------------------Extract data from data text file
+data = dlmread("save_list_colors.txt",'') #-----------------------------------------Extract data from data text file
 meanOfData = meanValue(data); #------------------------------------------------------Sum all values of data; mean() error "mean(6): out of bound 3 (dimensions are 3x1) (note: variable 'mean' shadows function)"
 sDeviation = std(data')'; #----------------------------------------------------------Standard deviation function
 processedData= xToxPrime(data,meanOfData,sDeviation);#-------------------------------normalized raw RGB traces
-[delta,rowN]=findMinMax(processedData); #--------------------------------------------returns biggest difference min-max value and corresponding row number
-validSignal=processedData(rowN,1:1:columns(processedData));#-------------------------equation (3)
-heartBeatssignal=fft(validSignal);#--------------------------------------------------fast fourier transform
+heartBeatssignal=abs(fft(processedData(2, 1:750)));#--------------------------------------------------fast fourier transform
+Fs = 25;
+f = Fs/2*linspace(0,1,length(heartBeatssignal));
 plot(0:1:length(heartBeatssignal)-1,heartBeatssignal(:))#----------------------------plot
